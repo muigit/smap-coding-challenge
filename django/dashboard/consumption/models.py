0b831544b0d9bf5f16259633f18c1efead256c8a
muigit <108963390+muigit@users.noreply.github.com>
2023-09-10 18:06:53 +0900

    ENECHANGE-2023 smap-coding-challenge User,Consumptionモデル追加

--- django/dashboard/consumption/models.py
+++ django/dashboard/consumption/models.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+  id = models.IntegerField(primary_key=True)
+  area = models.CharField(max_length=2)
+  tariff = models.CharField(max_length=2)
+
+class Consumption(models.Model):
+  datetime = models.DateTimeField()
+  consumption = models.FloatField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
