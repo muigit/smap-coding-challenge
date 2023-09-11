3df04cf117e6ccd5311748a7893f648fc43759a3
muigit <108963390+muigit@users.noreply.github.com>
2023-09-11 19:58:47 +0900

    ENECHANGE-2023 smap-coding-challenge User,Consumption,AggregateUserDailyConsumption インデックス追加

--- django/dashboard/consumption/models.py
+++ django/dashboard/consumption/models.py
@@ -5,21 +5,21 @@ from django.db import models
 
 class User(models.Model):
   id = models.IntegerField(primary_key=True)
-  area = models.CharField(max_length=2)
-  tariff = models.CharField(max_length=2)
+  area = models.CharField(max_length=2, db_index=True)
+  tariff = models.CharField(max_length=2, db_index=True)
 
 class Consumption(models.Model):
-  datetime = models.DateTimeField()
+  datetime = models.DateTimeField(db_index=True)
   consumption = models.FloatField()
   user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 # ユーザーごとの日別集計
 class AggregateUserDailyConsumption(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
-  date = models.DateField()
+  date = models.DateField(db_index=True)
   total_consumption = models.FloatField()
   average_consumption = models.FloatField()
 
   class Meta:
-      # 特定の組み合わせが重複しない制約
+      # 特定の組み合わせの一意性制約
       unique_together = ('user', 'date')
